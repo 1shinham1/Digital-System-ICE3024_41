fa8248f3c8a947fc7e7630583ab0af4b09ccb732
신승헌 <shshin030601@gmail.com>
2026-09-12 14:14:39 +0900

    remove .v file

--- week1/nand4_op2.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns/1ps
-
-module nand4_op2 (
-    input [3:0] a,
-    output y
-);
-    assign y = ~&a;
-endmodule
--- week1/nor_op.v
+++
@@ -1,7 +0,0 @@
-module nor_op (
-    input [3:0] a,
-    input [3:0] b,
-    output [3:0] y
-);
-    assign y = ~(a|b);
-endmodule
